c901f5c816e0a8ecbd5a68aa3eaa2306e09198aa
George Howard <george.howard@spsmail.cuny.edu>
2014-09-29 19:26:41 -0400

    Saving file

--- task_01.py
+++ task_01.py
@@ -6,23 +6,21 @@ Created on Mon Sep 29 18:01:46 2014
 
 @author: ghhoward
 """
+#import data
 
-from data import numbers
+numbers = [0,1,2,3,4,5,6,7,8,9]
 
-def evens_and_odds(num, show_even=True):
+def evens_and_odds(numbers, show_even=True):
     """ 
-    
+    return_x
     """
-    for x in num:
-        if x%2 == 0:
-            even_num.append(x)
+    x = 0
+    return_x = []
+    for x in numbers:
+        if show_even and (x%2 == 0):        
+            return_x.append(x)
         else:
-            odd_num.append(x)
-            
-        if show_even:
-            return even_num[0:]
-        else:
-            return odd_num[0:]   
-
+            return_x.append(x)
+   
 print evens_and_odds(numbers)
 print evens_and_odds(numbers, False)
