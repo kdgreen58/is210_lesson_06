c8df5502a0c764b8ef27e565d479704420b1c234
George Howard <george.howard@spsmail.cuny.edu>
2014-10-04 21:59:14 -0400

    All basic code done

--- task_04.py
+++ task_04.py
@@ -5,8 +5,23 @@ import data
 
 SALT = 'monosodium-glutamate'
 
-def test_passwords()
+def test_passwords(account):
+    user_id = []
+    pass_hash = []
+    hashword = ''
 
-def crack_it()
+    for user_id in account:
+        pass_hash = user_id.split(':')
+        hash_word = crack_it(pass_hash[1])
+        print pass_hash[4], pass_hash[1]
 
-def report()
+def crack_it(hash_w):
+    for crack in data.WORDS:
+        if hash_w == data.crypt(crack, SALT):
+            print crack, hash_w
+            print '\n'
+            
+    
+#def report():
+
+test_passwords(data.PASSWD)
