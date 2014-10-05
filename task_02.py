c0ce0d0f7161f0bac27c9ea2de263bc5158ecfb3
Joanne Garcia <joanne.garcia@spsmail.cuny.edu>
2014-10-05 19:00:03 -0400

    ehh?

---
+++ task_02.py
@@ -0,0 +1,41 @@
+#!usr/bin/env python
+# -*- coding: utf-8 -*-
+"""DOCSTRING"""
+
+import task_01
+import data
+
+
+def get_average(listobj):
+    """Determines the average of a list object of integers.
+
+        Args:
+            listobj (list): list of integers
+
+        Returns:
+            an average (mean) of the integers in a list
+
+        Examples:
+            >>> get_average([1,2,3,4,5])
+            3.0
+    """
+    total = 0
+    for x in listobj:
+        total = (total+x)
+    AVG = float(total/len(listobj))
+    return AVG
+
+TOTAL_AVG = "{0:,}".format(float("%.2f" % get_average(data.TASK_O1)))
+EVEN_AVG = "{0:,}".format(float("%.2f" % get_average(
+    task_01.evens_and_odds(data.TASK_O1))))
+ODD_AVG = "{0:,}".format(float("%.2f" % get_average(
+    task_01.evens_and_odds(data.TASK_O1, show_even=False))))
+
+REPORT =  """
+Task 02 Report
+_________________________
+Total AVG: {0}
+Even AVG:  {1}
+Odd AVG:   {2}"""
+
+print REPORT.format(TOTAL_AVG, EVEN_AVG, ODD_AVG)
