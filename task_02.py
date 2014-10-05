f348fed5bc7b648e6537b4726c08e8dd1073d5cb
apag101 <apag101@yahoo.com>
2014-10-05 15:35:26 -0400

    task02.1

--- task_02.py
+++ task_02.py
@@ -7,7 +7,7 @@ import data
 import task_01
 
 def get_average(D):
-
+     
     """Returns Average of a list.
 
     Args:
@@ -19,23 +19,35 @@ def get_average(D):
     Examples:
    
     """
-    ENUMS = []
-    TOTAL_AVG = 0
+    global TOTAL_AVG
     L = len(D)
     A= 0
     for x in D:
         A = A + x
-      
     TOTAL_AVG = float(A / L)
-    #ENUMS = task_01.evens_and_odds(D)
-    #EVEN_AVG = get_average(ENUMS)
-    #EVEN_AVG = get_average(D)
-    #ODD_AVG = task_01.evens_and_odds(D, show_evens = False) 
-    
+    return TOTAL_AVG
+
+def REPORT(D):
+
+    """Returns Average of even and odd list.
+
+    Args:
+    numbers(numeric).
+
+    Returns:
+    Number: Returns report of average numbers.
+
+    Examples:
+   
+    """
+
+    EVEN_AVG = get_average(task_01.evens_and_odds(D))
+    ODD_AVG = get_average(task_01.evens_and_odds(D, False))
+    TOTAL_AVG = get_average(D)
+        
     REPORT = '''    Task 02 Report
     -----------------------------
-    Total AVG:      {T:,.2f} '''.format(T=TOTAL_AVG, E=EVEN_AVG)
-    #EVEN_AVG:       {E:,.2f} 
-   # ODD_AVG:        {O:,.2f}'''
-    
-    return REPORT
+    Total AVG:      {T:,.2f}
+    EVEN_AVG:       {E:,.2f}
+    ODD_AVG:        {O:,.2f}'''.format(T=TOTAL_AVG, E=EVEN_AVG, O=ODD_AVG)
+    print REPORT
