e3521074be1a558600f940b9fbcc8614d123b4cd
pwengerd <pwengerd@gmail.com>
2014-10-05 16:22:26 +0300

    task 3 violations

--- task_03.py
+++ task_03.py
@@ -6,15 +6,26 @@ import data
 
 
 def bubble_sort(bubblelist):
+    """accepts a list of integers and orders
+    in ascending order
+
+    Args
+    bubblelist (list) : list of integers to be
+        returned in ascending order
+
+    Returns:
+    list: list of integers in ascending order
+    """
     listlength = len(bubblelist)
-    for i in range(0,listlength):
+    for i in range(0, listlength):
         moved = False
         for value in range(0, listlength-i-1):
             if bubblelist[value] > bubblelist[value + 1]:
                 holder = bubblelist[value + 1]
                 bubblelist[value + 1] = bubblelist[value]
                 bubblelist[value] = holder
-                swapped = True
-        if not moved: break
+                moved = True
+        if not moved:
+            break
 
     return bubblelist
