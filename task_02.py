eedef55d448554ba14198304f74bee4e01da72c6
Sam Cohen-DeVries <samuel.cohen-devries@spsmail.cuny.edu>
2014-10-06 02:30:59 -0400

    Updated Task 02

--- task_02.py
+++ task_02.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""Computes averages, including broken down by even and odd values"""
 
 import task_01
 import data
+import decimal
 def get_average(nums):
-    total = 0
-    count = 0
+    total = 0.00000000000000000000
+    count = 0.00000000000000000000
     for i in nums:
         total += i
         count += 1
-    return float(total / count)
+    return total / count
 
 TOTAL_AVG = get_average(data.TASK_O1)
 EVEN_AVG = get_average(task_01.evens_and_odds(data.TASK_O1))
@@ -18,5 +20,5 @@ ODD_AVG = get_average(task_01.evens_and_odds(data.TASK_O1, show_even = False))
 import decimal
 print "Task 02 Report\n------------------------\n\
 Total AVG: {0}\nEven AVG: {1}\n\
-Odd AVG: {2}".format(decimal.Decimal(TOTAL_AVG), \
-                     decimal.Decimal(EVEN_AVG), decimal.Decimal(ODD_AVG))
+Odd AVG: {2}".format(TOTAL_AVG, \
+                     EVEN_AVG, ODD_AVG)
