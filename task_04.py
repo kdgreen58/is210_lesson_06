974f8462139d6cbc1ab8d13387940c68af7f8628
Sam Cohen-DeVries <samuel.cohen-devries@spsmail.cuny.edu>
2014-10-06 08:47:50 -0400

    Updated Task 04

--- task_04.py
+++ task_04.py
@@ -1,26 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""Generates password cracks"""
 
 import data
 SALT = 'monosodium-glutamate'
 def test_passwords(users):
-    for i in range(len(users)):
+    for i in range(0, len(users)):
         cracked_pws = []
-        pws = users[i].split(':')
+        pws = [users[i].split(':')[0], users[i].split(':')[1]]
         cracked = crack_it(pws)
-        if cracked == pws[1]:
-            cracked_pws.append(pws[0], pws[1])
+        if cracked:
+            cracked_pws.append(users[i].split(':')[0], users[i].split(':')[1])
             users = cracked_pws
         return users
 
 def crack_it(pws):
     words = test_passwords(pws)
-    users = []
+    users = ()
     for i in range(0, len(data.WORDS)):
         x = data.crypt(data.WORDS[i], SALT)
-        if x in words:
-            users.append(x)
-    return users
+        if x[1] in words:
+            users.append([x])
+        return users
 
 def report(users):
     print "Cracked passwords\n-------------\n{0}".format(test_passwords(data.WORDS))
