74d79be1275804a450c83b1dfc9719fe66ca4a00
pwengerd <pwengerd@gmail.com>
2014-10-06 08:54:35 +0300

    fix decimal and commas

--- task_02.py
+++ task_02.py
@@ -5,6 +5,7 @@
 import data
 import task_01
 
+
 def get_average(integerlist):
     """Produces the average of a list of integers
 
@@ -22,6 +23,7 @@ def get_average(integerlist):
     averagelist = float(sumlist / lengthlist)
     return averagelist
 
-TOTAL_AVERAGE = get_average(data.TASK_O1)
-EVEN_AVERAGE = get_average(task_01.evens_and_odds(data.TASK_O1, show_even=True))
-ODD_AVERAGE = get_average(task_01.evens_and_odds(data.TASK_O1, show_even=False))
+
+TOTAL_AVERAGE = get_average(data.TASK_O1).__format__("0,.2f")
+EVEN_AVERAGE = get_average(task_01.evens_and_odds(data.TASK_O1, show_even=True)).__format__("0,.2f")
+ODD_AVERAGE = get_average(task_01.evens_and_odds(data.TASK_O1, show_even=False)).__format__("0,.2f")
