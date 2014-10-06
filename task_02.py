f0e6f1f1f2e3ce70c0c3bf09b8d554698b5e9dad
pwengerd <pwengerd@gmail.com>
2014-10-06 11:02:24 +0300

    float returned

--- task_02.py
+++ task_02.py
@@ -21,11 +21,11 @@ def get_average(integerlist):
     for n in integerlist:
         sumlist = Decimal(n + sumlist)
     lengthlist = len(integerlist)
-    averagelist = Decimal(sumlist / lengthlist)
+    averagelist = float(sumlist / lengthlist)
     return averagelist.__format__("0.6f")
 
 
 TOTAL_AVERAGE = get_average(data.TASK_O1)
 EVEN_AVERAGE = get_average(task_01.evens_and_odds(data.TASK_O1, show_even=True))
 ODD_AVERAGE = get_average(task_01.evens_and_odds(data.TASK_O1, show_even=False))
-print EVEN_AVERAGE
+print TOTAL_AVERAGE
