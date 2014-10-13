174096a8f02fde431c650063fc7f8e27b52a27cb
Kolton Green <kolton.green@spsmail.cuny.edu>
2014-10-13 00:19:19 -0400

    My code for task 03.

---
+++ task_03.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""My task 03 code."""
+
+import data
+
+def bubble_sort(numbers):
+    """Puts a list of numbers in order. 
+
+    Args:
+        numbers(num): A numeric type that reads in numbers.
+
+    Returns:
+        list: returns a list in numerical order
+
+    Example:
+
+        >>>bubble_sort([3, 2, 1])
+        [1, 2, 3]
+    """
+    length = len(numbers)
+    for value in range (length - 1, 0, -1):
+        for i in range(0, value):
+            if numbers[i] > numbers[i + 1]:
+                value = numbers[i]
+                numbers[i] = numbers[i + 1]
+                numbers[i + 1] = value
+
+    return numbers
